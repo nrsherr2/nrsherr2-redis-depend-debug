f7a218be912c4f999b5795b5c00609d649e1c4b0
Nicholas Sherrill <nsherrill@messerfinancial.com>
2022-12-13 10:18:36 -0500

    make it more spring-y

--- src/main/kotlin/com/example/nrsherr2redisdependdebug/SecurityConfiguration.kt
+++ src/main/kotlin/com/example/nrsherr2redisdependdebug/SecurityConfiguration.kt
@@ -8,12 +8,14 @@ import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.core.RedisOperations
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.core.session.SessionRegistry
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.session.FindByIndexNameSessionRepository
 import org.springframework.session.Session
-import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession
 import org.springframework.session.data.redis.RedisIndexedSessionRepository
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
+import org.springframework.session.security.SpringSessionBackedSessionRegistry
 
 
 @Configuration
@@ -21,28 +23,46 @@ class SecurityConfiguration() {
     @Autowired
     private lateinit var sessionRepository: FindByIndexNameSessionRepository<out Session>
 
-}
-
-@Configuration
-@EnableRedisHttpSession
-class SessionConfig(redisConnectionFactory: ObjectProvider<RedisConnectionFactory?>) {
-    private val redisConnectionFactory: RedisConnectionFactory?
-
-    init {
-        this.redisConnectionFactory = redisConnectionFactory.ifAvailable
+    @Bean
+    protected fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .and()
+            .sessionManagement()
+            .maximumSessions(1)
+            .sessionRegistry(sessionRegistry())
+        return http.build()
     }
 
     @Bean
-    fun sessionRedisOperations(): RedisOperations<String, Any> {
-        val redisTemplate = RedisTemplate<String, Any>()
-        redisTemplate.setConnectionFactory(redisConnectionFactory!!)
-        redisTemplate.keySerializer = StringRedisSerializer()
-        redisTemplate.hashKeySerializer = StringRedisSerializer()
-        return redisTemplate
-    }
-
-    @Bean
-    fun redisSessionRepository(sessionRedisOperations: RedisOperations<String?, Any?>?): FindByIndexNameSessionRepository<*> {
-        return RedisIndexedSessionRepository(sessionRedisOperations)
+    fun sessionRegistry(): SessionRegistry {
+        return SpringSessionBackedSessionRegistry(sessionRepository)
     }
 }
+
+//@Configuration
+//@EnableRedisHttpSession
+//class SessionConfig(redisConnectionFactory: ObjectProvider<RedisConnectionFactory?>) {
+//    private val redisConnectionFactory: RedisConnectionFactory?
+//
+//    init {
+//        this.redisConnectionFactory = redisConnectionFactory.ifAvailable
+//    }
+//
+//    @Bean
+//    fun sessionRedisOperations(): RedisOperations<String, Any> {
+//        val redisTemplate = RedisTemplate<String, Any>()
+//        redisTemplate.setConnectionFactory(redisConnectionFactory!!)
+//        redisTemplate.keySerializer = StringRedisSerializer()
+//        redisTemplate.hashKeySerializer = StringRedisSerializer()
+//        return redisTemplate
+//    }
+//
+//    @Bean
+//    fun redisSessionRepository(sessionRedisOperations: RedisOperations<String?, Any?>?): FindByIndexNameSessionRepository<*> {
+//        return RedisIndexedSessionRepository(sessionRedisOperations)
+//    }
+//}
